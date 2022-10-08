76a0c57966af2e934ebf690def2f985068f337a1
Krunal01 <parmarkrunal09101@gmail.com>
2022-10-08 14:46:29 +0530

    sidebar__headerRight

---
+++ src/Sidebar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Avatar } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import IconButton from "@material-ui/core/IconButton";
+// import DonutLargeIcon from "@material-ui/core/icons/DonutLargeIcon";
+
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* <h1>Sidebar</h1> */}
+
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search"></div>
+      <div className="sidebar__chats"></div>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAHfqn_skSdZCnHDbeP3abyHQtof74ZlwI",
+  authDomain: "krunal-whats-app-clone.firebaseapp.com",
+  projectId: "krunal-whats-app-clone",
+  storageBucket: "krunal-whats-app-clone.appspot.com",
+  messagingSenderId: "34683224371",
+  appId: "1:34683224371:web:cd51acab4030b18f57be46",
+  measurementId: "G-297NJ6SGCP",
+};
