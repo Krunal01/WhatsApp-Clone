6ddfd4b34e11b5a4ca452a84dbd46faf0205bcb5
Krunal01 <parmarkrunal09101@gmail.com>
2022-10-16 16:02:16 +0530

    half done

--- src/Chat.js
+++ src/Chat.js
@@ -81,7 +81,11 @@ function Chat() {
       </div>
       <div className="chat_body">
         {messages.map((message) => (
-          <p className={`chat_message ${true && "chat_reciever"}`}>
+          <p
+            className={`chat_message ${
+              message.name === user.displayName && "chat_reciever"
+            }`}
+          >
             <span className="chat_name">{message.name}</span>
             {message.message}
             <span className="chat_timeStamp">
