52dddde652706d4fa1efd4ad27347802c2125f6b
Krunal01 <parmarkrunal09101@gmail.com>
2022-10-17 22:38:43 +0530

    Env added

--- src/ifirebase.js
+++ src/ifirebase.js
@@ -3,13 +3,13 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyAHfqn_skSdZCnHDbeP3abyHQtof74ZlwI",
-  authDomain: "krunal-whats-app-clone.firebaseapp.com",
-  projectId: "krunal-whats-app-clone",
-  storageBucket: "krunal-whats-app-clone.appspot.com",
-  messagingSenderId: "34683224371",
-  appId: "1:34683224371:web:cd51acab4030b18f57be46",
-  measurementId: "G-297NJ6SGCP",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
